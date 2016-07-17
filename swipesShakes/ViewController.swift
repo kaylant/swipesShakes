76121622558689bd5aac4ab0887e059d70023bfb
kaylant <kaylant@gmail.com>
2016-07-17 15:13:17 -0500

    finished app

--- swipesShakes/ViewController.swift
+++ swipesShakes/ViewController.swift
@@ -10,9 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // for swipes
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+        
+    }
+    
+    // for shakes
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        
+        if event?.subtype == UIEventSubtype.MotionShake {
+        
+            print("Device was shaken")
+            
+        }
+        
+    }
+    
+    
+    // set up similar to long press
+    func swiped(gesture: UISwipeGestureRecognizer) {
+        
+        // tests to see if gesture that calls this function is a swipe
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+        
+            // these are the only gestures that can be recognized
+            switch swipeGesture.direction {
+            
+            case UISwipeGestureRecognizerDirection.Right:
+                
+                print("Swiped Right")
+                
+            case UISwipeGestureRecognizerDirection.Up:
+                
+                print("Swiped Up")
+            
+            // will break the switch and end it
+            default:
+                
+                break
+            
+            }
+            
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
